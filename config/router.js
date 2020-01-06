020a9fa319308f56c8699bd4cd52b5c8075eb902
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-06 12:21:51 +0100

    router.js

--- config/router.js
+++ config/router.js
@@ -3,7 +3,8 @@
 const routes = [].concat(
   require('../routes/404'),
   require('../routes/post'),
-  require('../routes/user')
+  require('../routes/user'),
+  require('../routes/api')
 )
 
 module.exports = {
